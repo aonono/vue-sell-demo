8db2e9e6ceddf4a268cfda3dfbea9bbf9807bd99
aoping <1216791584@qq.com>
2017-03-29 10:02:12 +0800

    商品页头部
    布局、样式基本写好

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <v-header :seller="seller"></v-header>
+    <ul>
+      <li><router-link to="/goods">商品</router-link></li>
+      <li><router-link to="/ratings">评价</router-link></li>
+      <li><router-link to="/seller">商家</router-link></li>
+    </ul>
+    <router-view v-bind:seller="seller"></router-view>
+    <i class="icon-keyboard_arrow_right"></i>
+  </div>
+</template>
+
+<script>
+import header from './components/header/header'
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      seller: {}
+    }
+  },
+  components: {
+    'v-header': header
+  },
+  created(){
+    this.$http.get('static/data.json').then((res) => {
+      this.seller = res.body.seller
+    })
+  }
+}
+
+</script>
+
+<style>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,44 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import Router from 'vue-router'
+import VueResource from 'vue-resource'
+import App from './App'
+import goods from './components/goods/goods'
+import seller from './components/seller/seller'
+import ratings from './components/ratings/ratings'
+import './common/stylus/index.styl'
+import '../static/css/reset.css'
+// Vue.config.productionTip = false
+Vue.use(Router)
+Vue.use(VueResource)
+const routes = [{
+  path: '/',
+  name: 'goods',
+  component: goods
+}, {
+  path: '/goods',
+  name: 'goods',
+  component: goods
+}, {
+  path: '/seller',
+  name: 'seller',
+  component: seller
+}, {
+  path: '/ratings',
+  name: 'ratings',
+  component: ratings
+}]
+
+var router = new Router({
+  linkActiveClass: 'active',
+  routes: routes
+})
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
